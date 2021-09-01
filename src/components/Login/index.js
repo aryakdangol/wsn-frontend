0789624b5f9ad9d63b299b4fc212f5449e972a7f
Sachin Kharel <sachink1630@gmail.com>
2021-09-01 21:08:25 +0545

    test

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-import React,{useState} from 'react'
-import { LoginBox, LoginContainer, LoginHeader, Password, PhoneNum, SignUp, Submit, Username } from './loginElements'
-
-const Login = ({Clicked}) => {
-    const[isSignUp, setSignUp] = useState(false)
-
-    const showSignUp = (e) => {
-        e.preventDefault();
-        setSignUp(!isSignUp);
-      };
-    let head = (isSignUp ? 'SIGNUP':'LOGIN');
-    return (
-        <LoginContainer Clicked={Clicked}>
-            <LoginBox>
-                <LoginHeader>
-                    {head}
-                </LoginHeader>
-                
-                <Username type = "text" placeholder= "Username">
-                </Username>
-
-                <PhoneNum isSignUp ={isSignUp} type = "tel"  placeholder= "Phone number">
-                </PhoneNum>
-
-                <Password type = "password"  placeholder= "Password">
-                </Password>
-
-                <Submit type = "submit" >
-                    Login
-                </Submit>
-
-                <SignUp onClick={(e) => showSignUp(e)}>
-                    SignUp
-                </SignUp>
-
-            </LoginBox>
-        </LoginContainer>
-        
-    )
-}
-
-export default Login
-=======
 import React, { useState } from "react";
 import {
   LoginBox,
@@ -88,4 +44,3 @@ const Login = ({ Clicked }) => {
 };
 
 export default Login;
->>>>>>> 757cd8b31d241e9743775532627cb3f50a349766
