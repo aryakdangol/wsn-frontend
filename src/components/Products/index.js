fd96f6cfd36defa17fa85b323976b25902e75f00
Sachin Kharel <sachink1630@gmail.com>
2021-09-02 21:17:29 +0545

    SearchBar-In-ProductPage-Added

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -28,23 +28,29 @@ const Products = () => {
         <Login Clicked={Clicked} Open={Open} />
         <Background Clicked={Clicked} Open={Open} />
         <Navbar toggle={toggle} Open={Open} />
+        <Container>
+        <Form.Select className=" mb-2 mt-4">
+          <option>Choose your location</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+        </Container>
         <Container fluid>
-          <Row xs={1} md={4} >
+          <Row xs={2} sm={2} md={3} lg={4}>
             {Array.from({ length: 8 }).map((_, idx) => (
               <Col>
-                <Card className="img-fluid mb-2 mt-4">
+                <Card className="img-fluid mb-2 mt-4" >
                   <Card.Img variant="top" src={url}  />
                   <Card.Body>
                     <Card.Title>Timro mero sath</Card.Title>
-                    <InputGroup className="mb-3">
+                    <InputGroup className="mb-2" >
                       <Form>
-                      <Form.Check type = 'checkbox' label = 'Pay for courier'/>
-                      <Form.Check type = 'checkbox' label = 'Pay for laundary'/>
+                      <Form.Check type = 'checkbox' label ='Pay for courier'/>
+                      <Form.Check type = 'checkbox' label ='Pay for laundary'/>
                       </Form>
-                      
-                      <Col><Button type="submit">Buy</Button>{' '}</Col>
-                      
                     </InputGroup>
+                      <Col><Button type="submit">Buy</Button>{' '}</Col>
                   </Card.Body>
                 </Card>
               </Col>
